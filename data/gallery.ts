d5705cd69dfaf42f74417b290a25269f3877bf0a
julemanddk <72082922+julemanddk@users.noreply.github.com>
2026-04-30 13:33:07 +0200

    Update site assets

--- data/gallery.ts
+++ data/gallery.ts
@@ -13,87 +13,87 @@ export const galleryByYear: GalleryYear[] = [
     year: 2025,
     images: [
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-1.png",
+        src: "/images/gallery/2025/project-1-1.png",
         alt: "Hill climb – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-2.png",
+        src: "/images/gallery/2025/project-1-2.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-3.png",
+        src: "/images/gallery/2025/project-1-3.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-4.png",
+        src: "/images/gallery/2025/project-1-4.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-5.png",
+        src: "/images/gallery/2025/project-1-5.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-6.png",
+        src: "/images/gallery/2025/project-1-6.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-1-7.png",
+        src: "/images/gallery/2025/project-1-7.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-2-1.png",
+        src: "/images/gallery/2025/project-2-1.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-2-2.png",
+        src: "/images/gallery/2025/project-2-2.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-2-3.png",
+        src: "/images/gallery/2025/project-2-3.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-3-1.png",
+        src: "/images/gallery/2025/project-3-1.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-3-2.png",
+        src: "/images/gallery/2025/project-3-2.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-3-3.png",
+        src: "/images/gallery/2025/project-3-3.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-3-4.png",
+        src: "/images/gallery/2025/project-3-4.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-3-5.png",
+        src: "/images/gallery/2025/project-3-5.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-4-1.png",
+        src: "/images/gallery/2025/project-4-1.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-4-2.png",
+        src: "/images/gallery/2025/project-4-2.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-5-1.png",
+        src: "/images/gallery/2025/project-5-1.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-5-2.png",
+        src: "/images/gallery/2025/project-5-2.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-5-3.png",
+        src: "/images/gallery/2025/project-5-3.png",
         alt: "Finish line – 2025",
       },
       {
-        src: "/tobias-portfolio/images/gallery/2025/project-5-4.png",
+        src: "/images/gallery/2025/project-5-4.png",
         alt: "Finish line – 2025",
       },
     ],
